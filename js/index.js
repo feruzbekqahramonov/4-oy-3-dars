4d971c00f0bf7605f5268f5f43d916a42f7ff03c
feruzbekqahramov <feqahramnov@mail.com>
2023-11-29 21:36:01 +0500

    homework finish

---
+++ js/index.js
@@ -0,0 +1,117 @@
+// 1-masala
+// let masala1 = 29;
+// let masala1b = 54;
+// let masala1o = (masala1 / 10)-masala1 % 10 / 10; 
+// let masala1otwoo = (masala1b
+//  / 10)-masala1b % 10 / 10;
+// let max;
+
+// if (masala1o > masala1otwoo) {
+//     m1max=masala1o;
+// }else {
+//     max=masala1otwoo;
+// }
+// console.log(max);
+
+// 2-masala
+// let masala2a = 715;
+// let max;
+// let masala2b = masala2a % 10; // 7
+// let masala2o = ((masala2a % 100) - masala2b) / 10; // 3
+// let y = (masala2a / 100) - (masala2a % 100) / 100; // 5
+
+// if (masala2b > masala2o) {
+//      max = masala2b;
+// } else {
+//      max = masala2o;
+// }
+// if (max < y) {
+//      max = y;
+// }
+
+// 3-masala
+// let masala3a = 283;
+// // let m3b = m3a % 10;
+// let masala3o = (masala3a % 100/10) - (masala3a % 10) / 10; 
+// let max;
+// if (masala3o % 3 == 0) {
+//     max='Rost';
+// }else {
+//     max="Yolg`on";
+// }
+// console.log(max);
+
+// 4-masala 
+// let misol1 = +prompt("tasodifiy son kiriting");
+// let misol2;
+// let misol3;
+// misol2 = misol1 / 100;
+// misol3 = (misol1 % 100) / 10;
+// misol1 = misol1 % 10;
+
+// if (misol1 > misol2) {
+//   // console.log (misol1)
+//   if (misol1 > misol3) {
+//     console.log(misol1);
+//   } else {
+//     console.log(misol3);
+//   }
+// } else {
+//   // console.log (misol2)
+//   if (misol2 > misol3) {
+//     console.log(misol2);
+//   } else {
+//     console.log(misol3);
+//   }
+// }
+
+// 5-masala
+// let masala5a = +prompt("Tasodifiy son kiriting");
+// let masala5b = +prompt("Tasodifiy son kiriting");
+// let masala5c = +prompt("Tasodifiy son kiriting");
+// let max;
+
+// if (masala5a > masala5b) {
+//     max = masala5a;
+// }
+// else {
+//     max = masala5b;
+// } if (masala5c > max) {
+//     max = masala5c;
+// }
+// console.log(max);
+
+// 6-masala
+// let masala6a = +prompt("Son kiriting");
+// let masala6b = +prompt("Son kiriting");
+// let masala6c = +prompt("Son kiriting");
+// let max;
+// if (masala6a > masala6b) {
+//   max = masala6b;
+// } else {
+//   max = masala6a;
+// }
+// if (masala6c > max) {
+//   max = masala6c;
+// }
+// console.log(max);
+
+// 7-masala
+// let masala7a = prompt("Son kiriting marhamat");
+// let masala7b = prompt("Son kiriting marhamat");
+
+// masala7a = parseFloat(masala7a);
+// masala7b = parseFloat(masala7b);
+
+// if (masala7a > masala7b) {
+//     console.log("Siz eng katta son:" + masala7a);
+// }
+//  else if (masala7b > masala7a) {
+//     console.log("Siz eng katta son:" + masala7b);
+// } 
+// else {
+//     console.log("Sonlar teng" + (masala7a + masala7b));
+// }
+
+// 8-masala
+// let masala8a = ;
